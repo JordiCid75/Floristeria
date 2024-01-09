93c07c0b29f297e45472583ed9facada62116890
Jordi Cid <jordicid1@gmail.com>
2024-01-09 13:02:32 +0100

    test2

--- src/main/java/Persistence/DataBase.java
+++ src/main/java/Persistence/DataBase.java
@@ -2,24 +2,33 @@ package Persistence;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import org.json.JSONObject;
 
 public class DataBase {
 
-   private ObjectMapper objectMapper;
-   private  File file;
+	private ObjectMapper objectMapper;
+	private File file;
 
-   private final String filePath = "src/main/resources/FlowerShop.json";
+	private final String filePath = "src/main/resources/FlowerShop.json";
 
-    public DataBase()
-    {
-        objectMapper = new ObjectMapper();
-        file = new File(filePath);
+	public DataBase() {
+		objectMapper = new ObjectMapper();
+		file = new File(filePath);
 
+	}
 
+	public void write(JSONObject jsonObj) {
+		try (FileWriter fileWriter = new FileWriter(this.filePath)) {
+			fileWriter.write(jsonObj.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 
-
-    }
-
+	}
 
 }
